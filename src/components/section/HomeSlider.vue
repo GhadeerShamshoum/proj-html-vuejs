4514ea4f6677fc824b9b601c1a9a7a36144633ca
Ghadeer <ghaora@gmail.com>
2022-02-10 11:02:37 +0200

    Demos+Sale- under navbar

--- src/components/section/HomeSlider.vue
+++ src/components/section/HomeSlider.vue
@@ -6,6 +6,20 @@
         <button class="buttonLeft"><a href="#">Explore recent Work</a></button>
         <button class="buttonRight"><a href="#">Get a quote today</a></button>
       </div>
+      <div class="containerBtn"> 
+        <div class="demoContainer">
+          <i class="fa-solid fa-folder-open"></i>
+          <div class="textUnderIcon">Demos</div>
+        </div>
+          <div class="demoContainer">
+          <div class="price">
+            <div class="dolar">$</div>
+            <span class="thePrice"><strong>39</strong></span>
+          </div>
+          <div class="textUnderIcon">On Sale</div>
+        </div>
+
+      </div>
     </section>
 </template>
 
@@ -35,6 +49,7 @@ section{
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    position: relative;
     h1{
       text-transform:uppercase;
       color: $white;
@@ -43,6 +58,40 @@ section{
     }
     h2{
       color: $brightSun;
+    }
+    .containerBtn{
+      position: absolute;
+      top:5px;
+      right: 5px;
+      
+
+      .demoContainer{
+        height: 40px;
+        width: 40px;
+        border-radius: 5px;
+        background-color: $white;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: $blue;
+        margin-bottom: 5px;
+        .textUnderIcon{
+          font-size: 10px;
+        }
+        .price{
+          color: rgb(25, 192, 25);
+          display: flex;
+          height: 19px;
+          .dolar{
+            font-size: 10px;
+            margin-top: 0;
+          }
+          .thePrice{
+            font-size: 18px;
+          }
+        }
+      }
     }
 }
 
